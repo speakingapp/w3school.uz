96d9d852b3d28c6a8b4efc8d0d9dbe06558d1e16
speakingapp <143941090+speakingapp@users.noreply.github.com>
2023-09-22 05:33:17 +0000

    "main"

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +13,6 @@ export default defineConfig({
     }
   },
   integrations: [tailwind(), mdx()],
-  adapter: vercel()
+  output: 'static',
+  adapter: vercelStatic()
 });
